4b30a4211f3fec415f664c679cdf8f5ddba4555e
Adrian Ruiz <adrian@werblix.com>
2020-09-05 10:56:43 -0300

    seguimos con los cambios

---
+++ js/letras_maquina.js
@@ -0,0 +1,34 @@
+// Función texto maquina escribir hero Home
+
+var words = ['arquitectura', 'urbanismo', 'diseño'],
+    wordWrapper = document.getElementById('word'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+
+setInterval(function(){
+
+    if(wordWrapperContent.length > 0 && !addingWord ) {
+        wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+        wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+        addingWord = true;
+    }
+
+    if( addingWord ){
+        if( wordWrapperContent.length < words[counter].length  ) {
+        wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+        wordWrapperContent = wordWrapper.innerHTML;
+        } else {
+        if( counter < words.length) {
+            counter ++
+        }
+        addingWord = false;
+        }
+    }
+
+    if( counter == words.length) {
+        counter = 0;
+    }
+
+}, 200);
